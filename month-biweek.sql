2f58de6c0e20a6d06d1bb8eb74056922204d7303
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-07-31 01:25:51 -0500

    refactor: remove diff column

--- month-biweek.sql
+++ month-biweek.sql
@@ -2,7 +2,6 @@ SELECT
     ("half-" || biweek) as date_range,
     NUMBER_FORMAT(SUM(IF(type == "earning", amount, 0)), 2, ".", "") AS earnings,
     NUMBER_FORMAT(SUM(IF(type == "spending", amount, 0)), 2, ".", "") AS actuals,
-    NUMBER_FORMAT(SUM(IF(type == "budget", amount, 0)), 2, ".", "") AS budget,
-    NUMBER_FORMAT((earnings - actuals), 2, ".", "") AS diff
+    NUMBER_FORMAT(SUM(IF(type == "budget", amount, 0)), 2, ".", "") AS budget
 GROUP BY
     biweek
--- month.sql
+++ month.sql
@@ -2,7 +2,6 @@ SELECT
     "month" as date_range,
     NUMBER_FORMAT(SUM(earnings), 2, ".", "") AS earnings,
     NUMBER_FORMAT(SUM(actuals), 2, ".", "") AS actuals,
-    NUMBER_FORMAT(SUM(budget), 2, ".", "") AS budget,
-    NUMBER_FORMAT(SUM(diff), 2, ".", "") AS diff
+    NUMBER_FORMAT(SUM(budget), 2, ".", "") AS budget
 GROUP BY
     earnings
